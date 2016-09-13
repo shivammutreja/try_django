435d08e3f79a9c0726d3d85e6d7d721d984359eb
shivammutreja <shivammutreja25@gmail.com>
2016-09-13 11:37:43 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- myproject/settings.py
+++ myproject/settings.py
@@ -117,3 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/home/shivam/django_env/try_django/blog/static',
+)
